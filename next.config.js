da4efe48fd2ad625923a8f0ec9b8500f1b4b4b98
大谷東彦 <haruhiko@weseek.co.jp>
2022-08-04 13:56:54 +0000

    - add trailingSlash to config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const subDirectory = process.env.SUB_DIRECTORY
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: subDirectory,
+  basePath: subDirectory,
+  trailingSlash: subDirectory !== undefined
 }
 
 module.exports = nextConfig
