6b1493199ad7805296d71a41c8fe31f964a393e9
haruhikonyan <haruhikonyan@users.noreply.github.com>
2022-08-06 10:56:27 +0000

    deploy: 08b12ea1c9709cd9b4ce2a29bda64ea169feb536

---
+++ _next/static/u0HyuPtxUENMzTBKKcGWD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4501f489b52717ea.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
