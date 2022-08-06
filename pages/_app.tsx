08b12ea1c9709cd9b4ce2a29bda64ea169feb536
大谷東彦 <haruhiko@weseek.co.jp>
2022-08-06 10:55:33 +0000

    - 不要なもの削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>乗りレコ</title>
+        <meta
+          name='description'
+          content='何の曲のどんな乗り番を経験したか簡単マークアップ！乗りレコを共有して経験を共有しよう！'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className='container'>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
